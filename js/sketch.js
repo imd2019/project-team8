f9369b316543da4fe9a56f54e82b3d832654e95e
Eva Haugrund <58939941+EvaHaugrund@users.noreply.github.com>
2020-07-06 15:41:47 +0200

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -31,6 +31,7 @@ import {
   clubBackgroundPeople,
   hologram,
   evaluationSmiley,
+  vibration,
 } from "./p5setup.js";
 import {
   mentorSound1,
@@ -152,6 +153,7 @@ let e = 0;
 let m = 0;
 let c = 0;
 let a = 0;
+let v = 0;
 
 let surveilScore = 0;
 let happy = 0;
@@ -1613,6 +1615,7 @@ function draw() {
       image(phoneButtonImage, 1245, 630, 65, 100);
       break;
     case talkingHandler.empty11:
+      v++;
       image(messyRoomBackground, 15, 15, 1300, 600, 0, 0, 8300, 3300);
       image(characterButton, 20, 630, 100, 100);
       image(phoneButtonImage, 1245, 630, 65, 100);
@@ -1630,6 +1633,12 @@ function draw() {
       image(phoneText, 1245, 630, 65, 100);
       if (phoneButton.effect === true) {
         image(phoneTextEffect, 1243, 628, 69, 104);
+      }
+      if (v >= 10) {
+        image(vibration, 1233, 618, 88, 123);
+      }
+      if (v >= 20) {
+        v = 0;
       }
       if (phoneButton.triggered === true) {
         image(phoneBig, 510, 30, 300, 570); //gesperter Bildschirm
@@ -2012,7 +2021,7 @@ function draw() {
       image(messyRoomBackground, 15, 15, 1300, 600, 0, 0, 8300, 3300);
       image(characterButton, 20, 630, 100, 100);
       image(phoneButtonImage, 1245, 630, 65, 100);
-      evaluationButton.effect = false;
+      evaluationButton.triggered = false;
       break;
     case talkingHandler.hologramEvaluation:
       image(messyRoomBackground, 15, 15, 1300, 600, 0, 0, 8300, 3300);
@@ -2111,7 +2120,10 @@ function draw() {
   noFill();
   stroke(30, 30, 30);
   strokeWeight(10);
-  rect(10, 10, 1310, 610);
+  line(10, 10, 1320, 10);
+  line(10, 10, 10, 610);
+  line(1320, 10, 1320, 610);
+  // rect(10, 10, 1310, 610);
 }
 
 window.draw = draw;
