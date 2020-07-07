2de652db0ec9b382a6971336f7837c29f8793955
Marie-LouWeymann <58940117+Marie-LouWeymann@users.noreply.github.com>
2020-07-07 11:17:33 +0200

    Abspann geupdated

--- js/credits.js
+++ js/credits.js
@@ -17,8 +17,8 @@ export default class Credits {
     text("Hochschule Darmstadt", this.x, this.y - 100);
     text("TECHNIK & DESIGN", this.x, this.y + 150);
     text("MUSIC & SOUND", this.x, this.y + 500);
-    text("BILDER & ILLUSTRATIONEN", this.x, this.y + 1050);
-    text("DANKE!", this.x, this.y + 1700);
+    text("BILDER & ILLUSTRATIONEN", this.x, this.y + 1200);
+    text("DANKE!", this.x, this.y + 1900);
     //Untertitel
     textSize(20);
     text("Interactive Media Design", this.x, this.y - 50);
@@ -29,23 +29,35 @@ export default class Credits {
     text("Afeni Sodatonou", this.x, this.y + 300);
     text("Marie-Lou Weymann", this.x, this.y + 350);
     //Music & Sound
-    text("Youtube", this.x, this.y + 550);
-    text("Youtube", this.x, this.y + 600);
+    text(
+      "Kris Dekaro - Awake via https://www.youtube.com/watch?v=J1TrbHgyhEE",
+      this.x,
+      this.y + 550
+    );
+    text(
+      " Blue Moon - Cassette Tape via https://www.youtube.com/watch?v=9lO_31BP7xY&t=1s",
+      this.x,
+      this.y + 600
+    );
     text("https://incompetech.filmmusic.io/", this.x, this.y + 650);
     text(
-      "https://www.myinstants.com/instant/fbi-open-up-3322/",
+      " FBI open up via https://www.myinstants.com/instant/fbi-open-up-3322/",
       this.x,
       this.y + 700
     );
     text(
-      "https://www.zapsplat.com/music/lo-fi-glitch-1/",
+      "Übergang in die Parallelwelt via https://www.zapsplat.com/music/lo-fi-glitch-1/",
       this.x,
       this.y + 750
     );
-    text("Karl Casey @ White Bat Audio- Night Crawler", this.x, this.y + 800);
+    text(
+      "Karl Casey @ White Bat Audio- Night Crawler via https://www.youtube.com/watch?v=25Mcl0aTY5",
+      this.x,
+      this.y + 800
+    );
     text("Cassette Tape by Blue Moon", this.x, this.y + 850);
     text(
-      "V.I.P.N - Cyberpunk- Futuristic Wave Instrumental",
+      "V.I.P.N - Cyberpunk- Futuristic Wave Instrumental via https://www.youtube.com/watch?v=E9WewH9HXMA",
       this.x,
       this.y + 900
     );
@@ -54,19 +66,29 @@ export default class Credits {
       this.x,
       this.y + 950
     );
+    text(
+      " Klingeln der Haustüre via https://www.zapsplat.com/music/doorbell-ring-ding-hold-then-dong/",
+      this.x,
+      this.y + 1000
+    );
+    text(
+      "Sound der Sicherheitstür via https://www.zapsplat.com/music/science-fiction-door-open-electronic-whirr/",
+      this.x,
+      this.y + 1050
+    );
     //Bilder
     text(
       "Hintergünde via Freepik (https://www.freepik.com):",
       this.x,
       this.y + 1100
     );
-    text("upklayk", this.x, this.y + 1150);
-    text("vectorpocket", this.x, this.y + 1200);
-    text("pch.vector", this.x, this.y + 1250);
-    text("vectorpouch", this.x, this.y + 1300);
+    text("upklayk", this.x, this.y + 1250);
+    text("vectorpocket", this.x, this.y + 1300);
+    text("pch.vector", this.x, this.y + 1350);
+    text("vectorpouch", this.x, this.y + 1400);
     // text("", this.x, this.y + 780);
     //Timer
-    if (this.i >= 700) {
+    if (this.i >= 850) {
       image(hdaAbspann, 15, 15, 1300, 600, 0, 0, 1920, 900);
     }
   }
